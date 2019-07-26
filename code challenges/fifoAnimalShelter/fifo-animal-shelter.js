fc73bad6a4485f8678ee6543c6b78c48354b8c70
KlNGU <brandynvay@yahoo.com>
2019-07-25 21:29:50 -0700

    some code

---
+++ code challenges/fifoAnimalShelter/fifo-animal-shelter.js
@@ -0,0 +1,21 @@
+'use strict';
+
+class AnimalShelter {
+  constructor() {
+    this.dogQ = [];
+    this.catQ = [];
+  }
+
+  enqueue(animal) {
+    if (animal === 'cat') {
+      animal.push(catQ);
+    } else if (animal === 'dog') {
+      animal.push(dogQ);
+    }
+  }
+
+  dequeue(pref) {
+    
+  }
+
+}
